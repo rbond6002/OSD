93f1b206ada23d9ed18bf95a9a4d11e560ad9689
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-05-27 15:20:51 -0400

    Update osd.ps1

--- osd.ps1
+++ osd.ps1
@@ -2,4 +2,4 @@
 
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 
-StartOSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart

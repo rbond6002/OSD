cd473514f7f114e252d6d8d2e9644928ac210eeb
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-05-27 14:16:54 -0400

    Add files via upload

---
+++ osd.ps1
@@ -0,0 +1,5 @@
+# zti.osdcloud.ch - needs to be here for blog post on akosbakos.ch
+
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+
+StartOSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart

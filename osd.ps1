e1f576ac591a986afaaff33b3cdde5015998fc0b
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-05-30 10:38:54 -0400

    Update osd.ps1

--- osd.ps1
+++ osd.ps1
@@ -2,4 +2,4 @@
 
 Write-Host -ForegroundColor Green "Starting OSDCloud lite touch (confirm erase disk)"
 
-Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart
+Start-OSDCloud -OSName 'Windows 11 24H2' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart

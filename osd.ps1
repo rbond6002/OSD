d9fcce09394b040d40b673200201e3bdae204767
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-06-17 14:09:00 -0400

    Update osd.ps1
    add cleanup script

--- osd.ps1
+++ osd.ps1
@@ -1,7 +1,14 @@
 # OSD config
-
-Write-Host -ForegroundColor Green "Starting OSDCloud lite touch (confirm erase disk)"
+Write-Host -ForegroundColor Green "Starting OSDCloud lite touch (must confirm erase disk)"
 
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart
 
-Read-Host -Prompt "Press Enter to exit"
+# Create C:\Windows\Setup\Scripts\SetupComplete.cmd (automatically runs in OOBE)
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+PowerShell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+PowerShell.exe -Command "& { Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/rbond6002/OSD/refs/heads/main/SetupComplete.ps1') }"
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+Write-Host  -ForegroundColor Green "Restarting"

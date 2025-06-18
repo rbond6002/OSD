0b4e056975d5da33d90faf74bebbc991f3f96a01
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-06-18 12:35:36 -0400

    Update osd.ps1

--- osd.ps1
+++ osd.ps1
@@ -1,7 +1,14 @@
-# OSD config
-Write-Host -ForegroundColor Green "Starting OSDCloud lite touch (must confirm erase disk)"
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Education'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-us'
 
-Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Education -OSActivation Volume -Restart
+#Launch OSDCloud
+Write-Host "Starting OSDCloud lite touch (must confirm erase disk)" -ForegroundColor Green
+Write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -Restart"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -Restart
 
 # Create C:\Windows\Setup\Scripts\SetupComplete.cmd (automatically runs in OOBE)
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"

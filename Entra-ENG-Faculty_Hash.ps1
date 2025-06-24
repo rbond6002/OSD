1edc7b3d0df6eb948ddf4fd63d0c77c7d468463b
rbond6002 <61504324+rbond6002@users.noreply.github.com>
2025-06-24 13:48:06 -0400

    Create Entra-ENG-Faculty_Hash.ps1

---
+++ Entra-ENG-Faculty_Hash.ps1
@@ -0,0 +1,57 @@
+#OSDCloud Variables
+$OSName      = 'Windows 11 24H2 x64'
+$OSEdition   = 'Education'
+$OSActivation= 'Volume'
+$OSLanguage  = 'en-us'
+
+Write-DarkGrayHost "Copying PFX file & the import script"
+Copy-Item X:\OSDCloud\Config\Scripts C:\OSDCloud\ -Recurse -Force
+}
+
+# Launch OSDCloud
+Write-Host "Starting OSDCloud lite touch (must confirm erase disk)" -ForegroundColor Green
+Write-Host "Afterwards, it will add the device to Autopilot with Grouptag Entra-ENG-Faculty" -ForegroundColor Green
+Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -Restart"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -Restart
+
+# Create SetupComplete.cmd (runs at OOBE)
+Write-Host "Create C:\Windows\Setup\Scripts\SetupComplete.cmd" -ForegroundColor Green
+$SetupCompleteCMD = @'
+PowerShell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+PowerShell.exe -Command "& { Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://github.com/rbond6002/OSD/blob/main/cleanup.ps1') }"
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+# Build Unattend.xml with static path import & cleanup, with group tag variable
+$UnattendXml = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+  <settings pass="specialize">
+    <component name="Microsoft-Windows-Deployment"
+               processorArchitecture="amd64"
+               publicKeyToken="31bf3856ad364e35"
+               language="neutral"
+               versionScope="nonSxS"
+               xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State"
+               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <RunSynchronous>
+        <RunSynchronousCommand wcm:action="add">
+          <Order>1</Order>
+          <Description>Start Hash Import</Description>
+          <Path>PowerShell -ExecutionPolicy Bypass -File C:\OSDCloud\Scripts\Entra-ENG-Faculty-USB.ps1</Path>
+        </RunSynchronousCommand>
+      </RunSynchronous>
+    </component>
+  </settings>
+</unattend>
+'@
+
+# Ensure Panther folder and write Unattend.xml
+if (-not (Test-Path 'C:\Windows\Panther')) {
+    New-Item -Path 'C:\Windows\Panther' -ItemType Directory -Force | Out-Null
+}
+$Panther      = 'C:\Windows\Panther'
+$UnattendPath = Join-Path $Panther 'Unattend.xml'
+$UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Width 2000 -Force
+
+Write-Host "Restarting" -ForegroundColor Green
